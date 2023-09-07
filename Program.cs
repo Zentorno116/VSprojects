3348f4c51b8acdf8835e0265d5be6b7db31ccb3b
Timur <zamilov05@mail.ru>
2023-09-07 22:07:38 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Мир Труд Май");
+        Console.WriteLine("Мир");
+        Console.WriteLine("    Труд");
+        Console.WriteLine("         Май");
+    }
+}
